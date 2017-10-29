111383eba8749c559a0eaccd2d915af6d29165b7
ErrantBot <errantbot@tutanota.com>
2017-10-29 15:45:31 +0000

    Improvements to the add_user script

--- add_user.py
+++ add_user.py
@@ -1,13 +1,18 @@
+#!bin/python
 from app import db
 from app.models import User
+import argparse
+
+parser = argparse.ArgumentParser(prog='./add_user')
+parser.add_argument("-u", "--username", help="username to add to the database")
+parser.add_argument("-p", "--password", help="password to add to the database")
+args = parser.parse_args()
 
 # create temporary user account
-user_email = ''
-passwd = ''
 try:
-    admin_user = User(user_email, passwd)
+    admin_user = User(args.username, args.password)
     db.session.add(admin_user)
     db.session.commit()
-    print "SUCCESS: added user - {}".format(user_email)
+    print "SUCCESS: added user - {}".format(args.username)
 except Exception as e:
-    print "ERROR: cannot add user - {}\nSTACKTRACE:\n {}".format(user_email, e)
+    print "ERROR: cannot add user - {}\nSTACKTRACE:\n {}".format(args.username, e)
