366fd2de911db136338555eeec7ef7df3aa57b49
ErrantBot <errantbot@users.noreply.github.com>
2017-11-02 20:47:42 +0000

    Pull improvements to user management (#5)
    * Merged ostip
    
    * Committed user login logic
    
    * Improvements to the add_user script
    
    * Improved README
    
    Added overview, installation instructions and versioning information to README.md
    
    * Fixed typos
    
    * Added user administration logic

--- add_user.py
+++ add_user.py
@@ -1,18 +1,23 @@
 #!bin/python
 from app import db
-from app.models import User
+from app.models import Users
 import argparse
 
 parser = argparse.ArgumentParser(prog='./add_user')
 parser.add_argument("-u", "--username", help="username to add to the database")
 parser.add_argument("-p", "--password", help="password to add to the database")
+parser.add_argument("-r", "--role", choices=('admin', 'user'),
+                    help="specify user role")
 args = parser.parse_args()
 
-# create temporary user account
+# create user account
 try:
-    admin_user = User(args.username, args.password)
-    db.session.add(admin_user)
+    if args.role == 'admin':
+        user = Users(args.username, args.password, args.role)
+    else:
+        user = Users(args.username, args.password, args.role)
+    db.session.add(user)
     db.session.commit()
     print "SUCCESS: added user - {}".format(args.username)
 except Exception as e:
-    print "ERROR: cannot add user - {}\nSTACKTRACE:\n {}".format(args.username, e)
+    print "ERROR: cannot add user - {}\nERROR:\n {}".format(args.username, e)
