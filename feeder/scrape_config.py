492be854ad7067ddfd4bad1a8ada3db9c79879ba
errantbot <errantbot@users.noreply.github.com>
2017-11-30 21:20:45 +0000

    Merge improvements to scraper and additional configurations (#12)
    * Merged ostip
    
    * Committed user login logic
    
    * Improvements to the add_user script
    
    * Improved README
    
    Added overview, installation instructions and versioning information to README.md
    
    * Fixed typos
    
    * Added user administration logic
    
    * Removed feeds page link from base while waiting for fix
    
    * Committed web scraper and fixed compatibility with python2
    
    * Readded feed.json.example
    
    * fixes and improvements to scrape.py
    
    * Committed scraping proof of concept
    
    * Committed v.1.0.0-beta
    
    * Fixes and improvements to scraper and task scheduler
    
    * Added additional scraping configurations

---
+++ feeder/scrape_config.py
@@ -0,0 +1,44 @@
+import datetime
+from scrape import fix_url
+
+vxvault_config = [('http://vxvault.net/ViriList.php',
+                   "//div[@id='page']/table/tr[td]",
+                   {"date": lambda row: datetime.datetime.strptime(
+                    str(datetime.datetime.now().year) + "-" + row[0][0].text,
+                    "%Y-%m-%d"),
+                    "url": lambda row: fix_url(row[1][1].text.strip()),
+                    "md5": lambda row: row[2][0].text.strip(),
+                    "ip": lambda row: row[3][0].text.strip(),
+                    },
+                   "//a[text()='Next >']/@href",
+                   lambda row, item: (datetime.datetime.now() - item["date"]).days > 30
+                   )]
+
+zeus_config = [("https://zeustracker.abuse.ch/monitor.php?browse=binaries",
+                "//table[@class='table' and tr[@class='tabletitle']]/tr[not(@class='tabletitle')]",
+                {
+                    "date": lambda row: datetime.datetime.strptime(row[0].text, "%Y-%m-%d"),
+                    "url": lambda row: fix_url(row[1][0].text.strip()),
+                    "md5": lambda row: row[3].text.strip(),
+                }
+                )]
+
+malwaredomainlist_config = [("http://www.malwaredomainlist.com/mdl.php",
+                             "//table[@class='table' and tr[@class='tabletitle']]/tr[not(@class='tabletitle')]",
+                             {"date": lambda row: datetime.datetime.strptime(row[0][0].text, "%Y/%m/%d_%H:%M"),
+                              "url": lambda row: fix_url(row[1].text.strip()),
+                              "ip": lambda row: row[2].text.strip(),},
+                             "//table[@class='table']/preceding-sibling::center//a[last()-1]/@href",
+                             lambda row, item: (datetime.datetime.now() - item["date"]).days > 30
+                             )]
+
+malcode_config = [("http://malc0de.com/database/",
+                   "//table[@class='prettytable']/tr[td]",
+                   {"date": lambda row: datetime.datetime.strptime(row[0].text, "%Y-%m-%d"),
+                    "url": lambda row: fix_url(row[1].text.strip()),
+                    "md5": lambda row: row[6][0].text.strip(),
+                    "ip": lambda row: row[2][0].text.strip(),
+                    },
+                   "//a[text()='next']/@href"
+                   )]
+
--- tasks.py
+++ tasks.py
@@ -1,50 +1,20 @@
 #!bin/python
-from app import db, app
-from celery.signals import task_postrun
-import datetime
-from app import create_celery_app
-from celery.task.schedules import crontab
-from celery.decorators import periodic_task
-from celery.utils.log import get_task_logger
+from app import app
 from datetime import datetime
-from feeder.feed import Feed
 from feeder.parse import ParseScrapedData
-from feeder.scrape import get
-import requests
+from feeder.scrape_config import vxvault_config
+from feeder.scrape import scrape_source
 import schedule
 import time
 
-'''
-celery = create_celery_app(app)
-logger = get_task_logger(__name__)
 
-
-@celery.task
-def send_event_notification():
-    # send mail to the recipients
-    print "sent the mail"
-
-@periodic_task(run_every=(crontab(hour="*", minute="1", day_of_week="*")))
-def pull_feeds():
-    logger.info("Start task")
-    now = datetime.now()
-    f = Feed()
-    results = f.process_all()
-    logger.info("pulled feeds")
-    logger.info("Task finished: total inserted %i" % len(results))
-
-@task_postrun.connect
-def close_session(*args, **kwargs):
-    db.session.remove()
-'''
-
-
-def job():
-    parser = ParseScrapedData('', get(), 'vxvault.net')
+def jobs():
+    """Runs parsing jobs"""
+    parser = ParseScrapedData(scrape_source(vxvault_config), 'vxvault.net')
     parser.run()
 
 
-schedule.every().day.at("19:55").do(job)
+schedule.every().day.at("19:55").do(jobs)
 
 app.logger.info('scheduler started on {}'.format(datetime.now()))
 while True:
