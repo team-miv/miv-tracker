06a5df992dc294668e905952d234354cec0653a9
ErrantBot <errantbot@tutanota.com>
2018-01-21 18:40:04 +0000

    minor fix

--- app/__init__.py
+++ app/__init__.py
@@ -26,16 +26,8 @@ app.config["FILEUPLOAD_ALLOWED_EXTENSIONS"] = ["png", "jpg", "jpeg", "gif"]
 from app.models import Users
 from app import views, models
 
-engine = create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
-meta = MetaData()
-sql_storage = SQLAStorage(engine, metadata=meta)
-blog_engine = BloggingEngine(app, sql_storage)
-login_manager = LoginManager(app)
-meta.create_all(bind=engine)
-
 
 @login_manager.user_loader
-@blog_engine.user_loader
 def load_user(user_id):
     return Users.query.filter(Users.id == int(user_id)).first()
 
--- run.py
+++ run.py
@@ -1,5 +1,20 @@
 #!bin/python
-from app import app
+from app import app, login_manager
+from app.models import Users
+
+from flask_blogging import SQLAStorage, BloggingEngine
+from sqlalchemy import create_engine, MetaData
+
+engine = create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
+meta = MetaData()
+sql_storage = SQLAStorage(engine, metadata=meta)
+blog_engine = BloggingEngine(app, sql_storage)
+login_manager = login_manager
+meta.create_all(bind=engine)
+
+@blog_engine.user_loader
+def load_user(user_id):
+    return Users.query.filter(Users.id == int(user_id)).first()
 
 if __name__ == '__main__':
     app.run(debug=True)
